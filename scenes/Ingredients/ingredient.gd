226c7246dbb47dea5e2f99cc5090266919e7a295
dave-29-dgm <nonnewtoniangamer@gmail.com>
2026-05-05 13:56:50 -0400

    Added Ingredient Spawner and Added Moving Ingredient Functionality
    Not sure if this is where we want to go but did some quick programming over lunch.  Added a spawner and an option to make ingredients move along a conveyor belt that you can grab from

--- scenes/Ingredients/ingredient.gd
+++ scenes/Ingredients/ingredient.gd
@@ -5,20 +5,33 @@ extends StaticBody3D
 
 var isHeld = false
 var isHovered = false
+var move_on_start = false
+var despawn = false
+var time_to_despawn = 6
+var move_speed = 2
+var move_direction = Vector3(1,0,0)
 var original_size;
 var bread
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	original_size = scale
+	#print("new ingredient")
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if move_on_start:
+		position = position + (move_direction*move_speed*delta)
+		time_to_despawn = time_to_despawn-delta 
+		if time_to_despawn <= 0:
+			queue_free()
 	pass
 	
 func on_grab():
+	move_on_start = false
+	despawn = false
 	pass
 	#$MeshInstance3D/GPUParticles3D.restart()
 	#$MeshInstance3D/GPUParticles3D.emitting = true
---
+++ scenes/Ingredients/ingredient_spawner.gd
@@ -0,0 +1,25 @@
+extends Node3D
+var ingredient_scene = preload("res://scenes/Ingredients/Ingredient.tscn")
+var spawn_wait_time = 1.8
+var timer = 0
+var moving_ingredients = true
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	timer = timer + delta
+	if timer >= spawn_wait_time:
+		#print("here")
+		timer = 0
+		var ingredient = ingredient_scene.instantiate()
+		print(position)
+		
+		get_parent().add_child(ingredient)
+		ingredient.position = position
+		ingredient.move_on_start = moving_ingredients
+		ingredient.despawn = true
+	pass
