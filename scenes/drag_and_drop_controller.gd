d9a581cc8bdeeac36b316c3e9e5416cf3242461f
dave-29-dgm <nonnewtoniangamer@gmail.com>
2026-05-06 21:23:54 -0400

    Added Music

--- scenes/drag_and_drop_controller.gd
+++ scenes/drag_and_drop_controller.gd
@@ -22,6 +22,7 @@ var firstPress = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	#@$AudioStreamPlayer.play()
 	pass # Replace with function body.
 
 
