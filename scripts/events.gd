6e3601cd32ba33843e77773cb03645aff7ec6d80
Gabriel <gdyer8899@gmail.com>
2026-05-07 17:24:34 -0500

    Cleaning up files

--- scripts/events.gd
+++ scripts/events.gd
@@ -1,7 +1,13 @@
 extends Node
 
+#Level Info for HUD
 signal ingredients_wasted
+signal time_left
 
 # Loss Conditions
 signal max_ingredients_wasted
 signal level_timer_end
+
+# Sandwich Info
+signal bad_sandwich
+signal good_sandwich
