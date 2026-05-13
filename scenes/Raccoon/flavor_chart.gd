cbd8145afd7e93005eed7a535709456f44fabc86
Gabriel <gdyer8899@gmail.com>
2026-05-13 15:05:40 -0500

    notches in bar charts

--- scenes/Raccoon/flavor_chart.gd
+++ scenes/Raccoon/flavor_chart.gd
@@ -7,15 +7,22 @@ var targets: Array = []
 @export var bar_color: Array[Color] = [ Color.RED, Color.BLUE, Color.GREEN]
 @export var spacing: float = 20.0
 @export var max_chart_value: float = 5.0
-@export var testing_chart: bool = true
+@export var testing_chart: bool = false
+
+# spacing
+@export var notch_outline_color: Color = Color.BLACK
+@export var notch_outline_width: float = 1.0
+@export var notch_gap: float = 5.0
+
 
 func update_chart_data(current_targets: Array)-> void:
-	if testing_chart:
-		current_targets = [1, 1, 1]
 	targets = current_targets
 	queue_redraw()
 
 func _draw() -> void:
+	if testing_chart:
+		targets = [4, 4, 4]
+		
 	if targets.is_empty():
 		return
 
@@ -24,11 +31,26 @@ func _draw() -> void:
 	var total_spacing: float = spacing * (num_bars + 1)
 	var bar_width: float = (size.x - total_spacing) / num_bars
 	
-	# create each bar
+	var notch_height: float = size.y / max_chart_value
+	var step_height: float = size.y / max_chart_value
+	
+	# Create each bar
 	for i in range(num_bars):
 		var x_pos: float = spacing + i * (bar_width + spacing)
-		var normalized_height = (targets[i] / max_chart_value) * size.y
-		var y_pos: float = size.y - normalized_height
-		var bar_rect: Rect2 = Rect2(x_pos, y_pos, bar_width, normalized_height)
+		var current_value: int = int(targets[i])
 		
-		draw_rect(bar_rect, bar_color[i])
+		# Loop through each individual unit notch for this bar
+		for notch_index in range(current_value):
+			var y_pos: float = size.y - ((notch_index + 1) * step_height)
+			
+			var final_y_pos: float = y_pos + notch_gap
+			var final_height: float = step_height - notch_gap
+			
+			if final_height <= 0:
+				continue
+				
+			var notch_rect: Rect2 = Rect2(x_pos, final_y_pos, bar_width, final_height)
+
+			draw_rect(notch_rect, bar_color[i % bar_color.size()])
+
+			draw_rect(notch_rect, notch_outline_color, false, notch_outline_width)
