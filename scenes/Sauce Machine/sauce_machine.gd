bb21ec0e66445e38bfda6166eeae428364d1b1b7
dave-29-dgm <nonnewtoniangamer@gmail.com>
2026-05-13 16:03:09 -0400

    Updating Sauce Mesh and colors to match charts

--- scenes/Sauce Machine/sauce_machine.gd
+++ scenes/Sauce Machine/sauce_machine.gd
@@ -3,7 +3,7 @@ extends StaticBody3D
 
 var sauce_scene = preload("res://scenes/Ingredients/Sauce.tscn")
 
-var colors = [ Color.RED,  Color.YELLOW, Color.BLUE, Color.WHITE]
+var colors = [ Color.RED, Color.BLUE,  Color.GREEN, Color.WHITE]
 var sauce_index = 0;
 var spawned = false;
 var timer = 1	
@@ -34,8 +34,7 @@ func _process(delta: float) -> void:
 	pass
 	
 func init_machine(index:int):
-	var material = StandardMaterial3D.new()
-	material.albedo_color = colors[index]
+	$sauceDispenser.update_color( colors[index])
 	#$sauceDispenser.set_surface_override_material(0, material)
 	sauce_index = index
 	
