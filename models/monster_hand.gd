aa9d39501160588de39937fa5bb0a204bd704dc1
alanbeyersdorf <52136663+alanbeyersdorf@users.noreply.github.com>
2026-05-12 23:16:35 -0400

    monster animation draft

---
+++ models/monster_hand.gd
@@ -0,0 +1,8 @@
+extends Node3D
+
+@onready var monsterArmAnim = $monsterPulseAnimation
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+	#monsterArmAnim.play("idle")
+	
